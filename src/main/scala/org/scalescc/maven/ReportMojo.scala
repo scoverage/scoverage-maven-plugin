6c4a45dcb16a0882d9a045fffcbd92b9a5f8e4ed
sksamuel <sam@sksamuel.com>
2013-11-21 09:12:07 +0000

    Removed prepare mojo, no need, now uses files

--- src/main/scala/org/scalescc/maven/ReportMojo.scala
+++ src/main/scala/org/scalescc/maven/ReportMojo.scala
@@ -5,31 +5,9 @@ import org.apache.maven.project.MavenProject
 import org.apache.maven.plugin.descriptor.PluginDescriptor
 import org.apache.maven.settings.Settings
 import org.apache.maven.plugin.AbstractMojo
-import scales.{InstrumentationRuntime, Env, IOUtils}
+import scales.InstrumentationRuntime
 
 /** @author Stephen Samuel */
-@Mojo(name = "prepare",
-  threadSafe = false,
-  requiresDependencyResolution = ResolutionScope.TEST,
-  defaultPhase = org.apache.maven.plugins.annotations.LifecyclePhase.PROCESS_TEST_CLASSES)
-class PrepareMojo extends AbstractMojo {
-
-  @Component
-  var project: MavenProject = _
-
-  @Component
-  var plugin: PluginDescriptor = _
-
-  @Component
-  var settings: Settings = _
-
-  def execute() {
-    getLog.info("Loading prepared coverage data [" + Env.coverageFile.getAbsolutePath + "]")
-    val coverage = IOUtils.deserialize(Env.coverageFile)
-    InstrumentationRuntime.setCoverage(coverage)
-  }
-}
-
 @Mojo(name = "report",
   threadSafe = false,
   requiresDependencyResolution = ResolutionScope.TEST,
@@ -47,6 +25,7 @@ class ReportMojo extends AbstractMojo {
 
   def execute() {
     getLog.info("Creating report")
+    getLog.info("Coverage" + System.identityHashCode(InstrumentationRuntime.coverage))
     getLog.info("Statements: " + InstrumentationRuntime.coverage.statements)
     getLog.info("Writing report [todo]")
   }
