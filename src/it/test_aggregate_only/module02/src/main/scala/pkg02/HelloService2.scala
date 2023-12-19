6d679931bbfa0cc34c03cba5b2826142612043c2
Eugene Platonov <jozic@live.com>
2023-12-18 21:52:01 -0500

    Add `aggregate` and `aggregateOnly` tests

---
+++ src/it/test_aggregate_only/module02/src/main/scala/pkg02/HelloService2.scala
@@ -0,0 +1,12 @@
+package pkg02
+
+class HelloService2
+{
+  def hello =
+  {
+    "Hello from module 2"
+  }
+
+}
+
+object HelloService2 extends HelloService2
---
+++ src/it/test_aggregate_only/module03/src/main/scala/pkg03/HelloService3.scala
@@ -0,0 +1,12 @@
+package pkg03
+
+class HelloService3
+{
+  def hello =
+  {
+    "Hello from module 3"
+  }
+
+}
+
+object HelloService3 extends HelloService3
