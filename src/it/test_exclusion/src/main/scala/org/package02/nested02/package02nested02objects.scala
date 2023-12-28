d44d55fd74f7005ecea6d77c40c068888ba360da
Eugene Platonov <jozic@live.com>
2023-12-28 15:25:42 -0500

    Add exclusion tests

---
+++ src/it/test_exclusion/src/main/scala/org/package02/nested02/package02nested02objects.scala
@@ -0,0 +1,14 @@
+// gonna be excluded from coverage report by package exclusion
+package org.package02.nested02
+
+object ImGonnaBeExcludedCauseImInPackage02Nested02 {
+
+  def hello: String = "Hello from package02.nested02"
+
+}
+
+object SameStoryBro {
+
+  def hello: String = "Hello from package02.nested02 again"
+
+}
