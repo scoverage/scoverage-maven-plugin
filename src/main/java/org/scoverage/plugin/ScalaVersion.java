d6378b7b423b0248f0b1d06e53a96f6ed3b5321f
Eugene Platonov <jozic@live.com>
2024-01-05 18:58:23 -0500

    Fix javadoc generation

--- src/main/java/org/scoverage/plugin/ScalaVersion.java
+++ src/main/java/org/scoverage/plugin/ScalaVersion.java
@@ -9,13 +9,42 @@ import java.util.regex.Pattern;
 public class ScalaVersion {
     private static final Pattern regexp = Pattern.compile("(\\d+)\\.(\\d+)(\\.\\d+)?([-\\.].+)?");
 
+    /**
+     * The full version number, including the modifier if any.
+     */
     public String full;
+
+    /**
+     * The binary compatible version number for this Scala version.
+     * e.g. for 2.10.0-M1, this would be 2.10.0-M1, for 2.10.0, this would be 2.10, for 3.3.1, this would be 3.
+     */
     public String compatible;
+
+    /**
+     * The major Scala version number. e.g. for 2.10.0-M1, this would be 2.
+     */
     public int major;
+
+    /**
+     * The minor Scala version number. e.g. for 2.10.0-M1, this would be 10.
+     */
     public int minor;
+
+    /**
+     * The bugfix Scala version number. e.g. for 2.10.0-M1, this would be 0.
+     */
     public int bugfix;
+
+    /**
+     * The modifier for this Scala version. e.g. for 2.10.0-M1, this would be M1.
+     */
     public String modifier;
 
+    /**
+     * Creates a ScalaVersion from a String.
+     *
+     * @param s String to parse
+     */
     public ScalaVersion(String s) {
         full = s;
         // parse
@@ -40,6 +69,9 @@ public class ScalaVersion {
 
     /**
      * Ignores modifier, so can return `true` for any ScalaVersion with matching major, minor, bugfix.
+     *
+     * @param other ScalaVersion to compare to
+     * @return true if this version is of the same or newer Scala version as the other version
      */
     public boolean isAtLeast(ScalaVersion other) {
         return major >= other.major &&
@@ -48,10 +80,20 @@ public class ScalaVersion {
 
     }
 
+    /**
+     * Ignores modifier, so can return `true` for any ScalaVersion with matching major, minor, bugfix.
+     *
+     * @param scalaVersion to compare to
+     * @return true if this version is of the same or newer Scala version as the other version
+     */
     public boolean isAtLeast(String scalaVersion) {
         return isAtLeast(new ScalaVersion(scalaVersion));
     }
 
+
+    /**
+     * @return true if this is a Scala 2 version
+     */
     public boolean isScala2() {
         return major == 2;
     }
