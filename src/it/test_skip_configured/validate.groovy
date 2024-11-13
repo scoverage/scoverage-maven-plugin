bf84b7df258ea30cf2de06749ae42cf023bfed9b
Eugene Platonov <jozic@live.com>
2024-11-13 14:07:11 -0500

    Don't compile main sources in `scoverage` lifecycle when `skip` is set

--- src/it/test_skip_configured/validate.groovy
+++ src/it/test_skip_configured/validate.groovy
@@ -5,6 +5,12 @@ try {
     assert lines.contains("[INFO] Skipping Scoverage execution as configured")
     assert !lines.contains("Generating \"SCoverage\" report")
 
+    // let's check main code is also not compiled
+    def scalaMainSourcesCompilerInfoIndex = lines.findIndexOf { it ==~ /.*:compile \(default-sbt-compile\).*/ } + 1
+    def scalaMainSourcesCompilerInfo = lines.get(scalaMainSourcesCompilerInfoIndex)
+    def expectedInfo = "[INFO] Not compiling main sources"
+    assert scalaMainSourcesCompilerInfo.contains(expectedInfo) : "Expected '$expectedInfo' but got: '$scalaMainSourcesCompilerInfo'"
+
     return true
 
 } catch (Throwable e) {
